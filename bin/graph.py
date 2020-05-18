bc67a9e26fb410d1ffd16f3f8deaa47efbecc7b9
Mueller <max-felix.mueller@egoproducts.com>
2020-05-18 09:49:13 +0200

    increased sampling speed by sampling first and mathing later. python code was slowing down the maximum sampling rate.

--- bin/graph.py
+++ bin/graph.py
@@ -1,6 +1,8 @@
 import serial
 import scipy.signal as signal
 
+import time
+
 ser = serial.Serial()
 ser.baudrate = 9600 # will set automatically
 ser.bytesize = serial.EIGHTBITS
@@ -12,19 +14,39 @@ input_buffer = []
 
 channels = [[], [], [], []]
 
-while len(channels[0]) < 100_000:
-    newbytes = ser.in_waiting
-    for _ in range(newbytes):
-        input_buffer.append(ser.read())
-        if input_buffer[0] == b'\x00':
-            if len(input_buffer) == 9:
-                channels[0].append(int.from_bytes(input_buffer[1], byteorder='big') | int.from_bytes(input_buffer[2], byteorder='big') << 8)
-                channels[1].append(int.from_bytes(input_buffer[3], byteorder='big') | int.from_bytes(input_buffer[4], byteorder='big') << 8)
-                channels[2].append(int.from_bytes(input_buffer[5], byteorder='big') | int.from_bytes(input_buffer[6], byteorder='big') << 8)
-                channels[3].append(int.from_bytes(input_buffer[7], byteorder='big') | int.from_bytes(input_buffer[8], byteorder='big') << 8)
+print(time.time())
+
+# sync
+while ser.read() != b'\x00':
+    pass
+
+for _ in range(1000000):
+    input_buffer.append(ser.read(9))
+    #print(ser.read(9))
+
+print(time.time())
+
+print(len(input_buffer))
+
+inputs = input_buffer.copy()
+input_buffer = []
+
+
+for inp in inputs:
+    try:
+        for bbyte in inp:
+            input_buffer.append(bbyte)
+            if input_buffer[0] == 0:
+                if len(input_buffer) == 9:
+                    channels[0].append(input_buffer[1] | input_buffer[2] << 8)
+                    channels[1].append(input_buffer[3] | input_buffer[4] << 8)
+                    channels[2].append(input_buffer[5] | input_buffer[6] << 8)
+                    channels[3].append(input_buffer[7] | input_buffer[8] << 8)
+                    input_buffer = []
+            else:
                 input_buffer = []
-        else:
-            input_buffer = []
+    except Exception as e:
+        pass
 
 import matplotlib.pyplot as plt
 
@@ -34,17 +56,21 @@ for channel in channels:
         vals.sort()
         channel[n] = vals[2]
 
+
+
+plt.plot(channels[0])
+
 tabs = 5001
 freq = 50
-sample_rate = 10_000
+sample_rate = 100_000
 tpeinh = signal.firwin(tabs, freq, pass_zero=True, fs=sample_rate)                                 # Filterkoeffizienten berechnen
 channels[0] = signal.lfilter(tpeinh, 1, channels[0])
 channels[1] = signal.lfilter(tpeinh, 1, channels[1])
 channels[2] = signal.lfilter(tpeinh, 1, channels[2])
 channels[3] = signal.lfilter(tpeinh, 1, channels[3])
 
-plt.plot(channels[0])
-plt.plot(channels[1])
-plt.plot(channels[2])
-plt.plot(channels[3])
+plt.plot(channels[0][2500:])
+#plt.plot(channels[1])
+#plt.plot(channels[2])
+#plt.plot(channels[3])
 plt.show()
